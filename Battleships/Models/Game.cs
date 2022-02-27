1ae990682a867f890c6f303f62e3a6782536d1e4
Marcin <m.pietrucha0@gmail.com>
2022-02-27 21:57:11 +0100

    game model added

---
+++ Battleships/Models/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Battleships.Models
+{
+    public class Game
+    {
+        public Player Player { get; set; }
+        public Player NPCPlayer { get; set; }
+
+        public Game()
+        {
+            Player = new Player("me");
+            NPCPlayer = new Player("PC");
+        }
+
+        public void RunGame()
+        {
+            while (Player.IsAlive == true && NPCPlayer.IsAlive == true)
+            {
+                PlayTurn();
+            }
+
+            if (Player.IsAlive == false)
+            {
+                Console.WriteLine($"{NPCPlayer.Name} won the game.");
+            }
+            else if (NPCPlayer.IsAlive == false)
+            {
+                Console.WriteLine($"{Player.Name} won the game.");
+            }
+        }
+
+        private void PlayTurn()
+        {
+
+        }
+    }
+}
