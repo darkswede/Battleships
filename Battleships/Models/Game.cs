72e37550245c958c77fd8df0afd3b88537def543
Marcin <m.pietrucha0@gmail.com>
2022-02-27 21:57:11 +0100

    game model added

---
+++ Battleships/Models/Game.cs
@@ -0,0 +1,28 @@
+﻿namespace Battleships.Models
+{
+    public class Game
+    {
+        public Player Player { get; }
+        public Player NPCPlayer { get; }
+
+        public Game()
+        {
+            Player = new Player("me");
+            NPCPlayer = new Player("PC");
+        }
+
+        public void PlayTurn(Coordinates coordinates)
+        {
+            Player.ManualShot(coordinates);
+            var shotResult = NPCPlayer.ProcessShot(coordinates);
+            Player.ProcessShotResult(shotResult);
+
+            if (NPCPlayer.IsAlive)
+            {
+                var shotAt = NPCPlayer.Fire();
+                shotResult = Player.ProcessShot(shotAt);
+                NPCPlayer.ProcessShotResult(shotResult);
+            }
+        }
+    }
+}
