d280ef864d091761222d4e4da0c2e0c91cc7390e
Marcin <m.pietrucha0@gmail.com>
2022-02-27 21:54:36 +0100

    base models - tile, ship, coordinates - added

---
+++ Battleships/Models/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace Battleships.Models
+{
+    public class Coordinates
+    {
+        public int Column { get; set; }
+        public int Row { get; set; }
+
+        public Coordinates(int column, int row)
+        {
+            Column = column;
+            Row = row;
+        }
+    }
+}
---
+++ Battleships/Models/Tile.cs
@@ -0,0 +1,28 @@
+﻿namespace Battleships.Models
+{
+    public class Tile
+    {
+        public Coordinates Coordinates { get; }
+        public bool IsOccupied { get; set; }
+        public bool Hit { get; protected set; }
+        public bool Miss { get; protected set; }
+
+        public Tile(int column, int row)
+        {
+            Coordinates = new Coordinates(column, row);
+            IsOccupied = false;
+            Hit = false;
+            Miss = false;
+        }
+
+        public void ChangeToHit()
+        {
+            Hit = true;
+        }
+
+        public void ChangeToMiss()
+        {
+            Miss = true;
+        }
+    }
+}
